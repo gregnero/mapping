78e7e5c6f50c20dc05ab455d41cb25242994e5ca
Gregory Nero <gnero@stsci.edu>
2018-06-26 15:35:36 -0400

    Bubble data as numpy array (MWP_ID, GLON, GLAT)

--- bubble_mapping.py
+++ bubble_mapping.py
@@ -3,3 +3,14 @@
 Map the large bubble data discussed in the paper presented by Simpson et al. to a given set of images of the galactic plane captured by Spitzer.
 
 """
+
+import pandas as pd
+import numpy as np
+
+#Read in the csv bubble data as a DataFrame. Change directory as needed.
+bubble_data = pd.read_csv("../Desktop/mapping_data/bubbly.csv")
+
+#Convert the DataFrame into a numpy array
+bubble_data = bubble_data.values
+
+
