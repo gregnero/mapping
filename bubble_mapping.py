70ddea5c5aed747e77c3c133a0048b0e5b54c9ba
Gregory Nero <gnero@stsci.edu>
2018-06-27 11:39:28 -0400

    Load image arrays into a list. Intend to use dict approach.

--- bubble_mapping.py
+++ bubble_mapping.py
@@ -6,6 +6,12 @@ Map the large bubble data discussed in the paper presented by Simpson et al. to 
 
 import pandas as pd
 import numpy as np
+import os
+from skimage import io
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import sys
+
 
 #Read in the csv bubble data as a DataFrame. Change directory as needed.
 bubble_data = pd.read_csv("../Desktop/mapping_data/bubbly.csv")
@@ -13,4 +19,13 @@ bubble_data = pd.read_csv("../Desktop/mapping_data/bubbly.csv")
 #Convert the DataFrame into a numpy array
 bubble_data = bubble_data.values
 
+#Initialize list to store image arrays
+spitzer_images = []
+
+#Load the image arrays into the list
+for file in sorted(os.listdir("../Desktop/mapping_data")):
+    if file.endswith(".jpg"):
+        with open(os.path.join("../Desktop/mapping_data", file), 'rb') as myfile:
+            spitzer_images.append(io.imread(myfile.name))
+            print("Status: Loading image %s" %file)
 
