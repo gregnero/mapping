9d43c8d777fae879940b25051dcfa211d2d3747a
Gregory Nero <gnero@stsci.edu>
2018-06-27 22:39:42 -0400

    Create the full image array that the dataset represents. I hope.

--- bubble_mapping.py
+++ bubble_mapping.py
@@ -4,16 +4,15 @@ Map the large bubble data discussed in the paper presented by Simpson et al. to
 a given set of images of the galactic plane captured by Spitzer.
 
 """
-
-import pandas as pd
-import numpy as np
 import os
-from skimage import io
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 import matplotlib
 import sys
 import collections
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from skimage import io
 
 #Read in the csv bubble data as a DataFrame. Change directory as needed.
 bubble_data = pd.read_csv("../Desktop/mapping_data/bubbly.csv")
@@ -36,5 +35,29 @@ for file in sorted(os.listdir("../Desktop/mapping_data")):
         image_names.append(file) 
         with open(os.path.join("../Desktop/mapping_data", file), 'rb') as myfile:
             image_dict[file] = io.imread(myfile.name)
-            print("Status: Loading image %s" %file)
+            print("Status: Loaded image %s" %file)
+
+#Initialize list to store all of the individual image arrays
+image_list_unordered = []
+
+#Put the image arrays from the dictionary of images into the initialized array
+for value in image_dict.values():
+    image_list_unordered.append(value)
+
+#Split previously created list of all of the images into their respective northgrid/
+#southgrid categories. They are considered "unsorted" because they don't yet align
+#spatially
+northgrid_unordered_list = image_list_unordered[0:22]
+southgrid_unordered_list = image_list_unordered[22:43]
+
+#Align the list of images in each grid spatially
+northgrid_list = northgrid_unordered_list[::-1]
+southgrid_list = southgrid_unordered_list[::-1]
+
+#Construct the spatially oriented northgrid and southgrid arrays
+northgrid = np.concatenate(northgrid_list, axis=1)
+southgrid = np.concatenate(southgrid_list, axis=1)
+
+#Stitch the northgrid and southgrid together to make a HUGE image
+final_panorama = np.concatenate((northgrid,southgrid), axis=1)
 
