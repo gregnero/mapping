304545e43fa7e4f14c5975623e9d266ee09b2ac7
Gregory Nero <gnero@stsci.edu>
2018-07-10 11:36:10 -0400

    Rearranged methods and made things tidy

--- cutoutgen.py
+++ cutoutgen.py
@@ -5,13 +5,125 @@ Author: Gregory M. Nero
 
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 import collections
 import os
 import sys
 from skimage import io
-import matplotlib.pyplot as plt
-
 from skimage.transform import resize, rescale
+
+def show_cutout_samples(cutout_dictionary, show_best, num_samples=12):
+    '''
+    Returns:
+    -------------
+    Figure with cutout samples
+
+    Args:
+    -------------
+    cutout_dictionary: dictionary; contains names as keys and numeric tuple as values
+    show_best: bool; if True, will show assortment of high hitrate bubbles
+                     if Fasle, will show random assortment of bubbles
+    num_samples: int; number of samples to show. Currently fixed at 12 for convenience
+
+    Usage:
+    -------------
+    show_cutout_samples(cutout_dict, show_best=True)
+
+    ''' 
+    
+    #Initialize lists to store dictionary information
+    cutout_names = []
+    cutout_images = []
+    cutout_radii = []
+    cutout_hitrates = []
+
+    #Set hitrate threshold value for show_best restriction
+    #Note: make sure at least 12 valid bubbles can be found...
+    threshold = 0.5
+    
+    #Fill the lists depending on value of show_best
+    for bub_name, bub_num in cutout_dictionary.items():
+        if show_best == True:
+            if bub_num[2] >= threshold:
+                cutout_names.append(bub_name)
+                cutout_images.append(bub_num[0])
+                cutout_radii.append(bub_num[1])
+                cutout_hitrates.append(bub_num[2])
+            else:
+                continue
+        else:
+            cutout_names.append(bub_name)
+            cutout_images.append(bub_num[0])
+            cutout_radii.append(bub_num[1])
+            cutout_hitrates.append(bub_num[2])
+
+    #Set up the figure 
+    fig = plt.figure()
+    if show_best == True:
+        title = str(num_samples) + " Samples with High Hitrate" 
+    else:
+        title = str(num_samples) + " Cutout Samples"
+    fig.suptitle(title, fontsize=18, x=0.5, y=0.986)
+    fig.subplots_adjust(hspace = 0.75)
+
+    #Plot the sample cutouts
+    for sample in range(1,13):
+        fig.add_subplot(3,4,sample)
+        plt.imshow(cutout_images[sample])
+        plot_title = cutout_names[sample] + "\n" + "(hitrate = " + str(cutout_hitrates[sample]) + ")"
+        plt.title(plot_title, fontsize=6)
+    print("Exit figure to continue...")
+    plt.show()
+
+
+def degree_to_pixel(glon, glat, reff, array):
+    '''
+    Returns:
+    ______________
+    tuple: (glon_idx, glat_idx, radius_in pixels)
+ 
+    Args:
+    --------------
+    glon: Floating. Galactic longitude. Unit: degrees. Expects range: [0:360]
+    glat: Floating. Galactic latitude. Unit: degrees. Expects range: [-1:1]
+    reff: floating. Unit: degrees.
+    array: a numpy ndarray
+    
+    Usage:
+    ______________
+    index = degree_to_pixel(220.34, -0.4, 0.0348, my_image_array)
+
+    '''
+
+    #Make sure input is good
+    if glon < 0 or glon > 360:
+        print('arg error: glon is out of acceptable range')
+    if glat < -1 or glat > 1:
+        print('arg error: glat is out of acceptable range')
+    if reff < 0:
+        print('arg error: radius must be positive')
+
+    #Regulate glon input to make the spherical wraparound less annoying
+    if glon <= 360 and glon >= 295.5:
+        glon = glon - 360
+
+    #Get array shape
+    rows = array.shape[0]
+    cols = array.shape[1]
+
+    #Set up the degree range for glon and glat based on the array shape
+    glat_range = np.linspace(1,-1,rows)
+    glon_range = np.linspace(64.5, -64.5, cols)
+
+    #Get the index based on closes value
+    glat_idx = (np.abs(glat_range - glat)).argmin()
+    glon_idx = (np.abs(glon_range - glon)).argmin()
+
+    #Get the effective pixel radius
+    radius_in_pixels = int(reff * 3000)
+
+    return (glon_idx, glat_idx, radius_in_pixels)
+
 
 #Read in the bubble csv with pandas and then convert to a numpy array
 bubble_data = (pd.read_csv("../Desktop/mapping_data/bubbly.csv")).values
@@ -71,53 +183,6 @@ if not 'final_panorama' in globals():
     final_panorama = np.concatenate((northgrid,southgrid), axis=1)
     print("Status: final image created.")
 
-def degree_to_pixel(glon, glat, reff, array):
-    '''
-    Returns:
-    ______________
-    tuple: (glon_idx, glat_idx, radius_in pixels)
- 
-    Args:
-    --------------
-    glon: Floating. Galactic longitude. Unit: degrees. Expects range: [0:360]
-    glat: Floating. Galactic latitude. Unit: degrees. Expects range: [-1:1]
-    reff: floating. Unit: degrees.
-    array: a numpy ndarray
-    
-    Usage:
-    ______________
-    index = degree_to_pixel(220.34, -0.4, 0.0348, my_image_array)
-
-    '''
-
-    #Make sure input is good
-    if glon < 0 or glon > 360:
-        print('arg error: glon is out of acceptable range')
-    if glat < -1 or glat > 1:
-        print('arg error: glat is out of acceptable range')
-    if reff < 0:
-        print('arg error: radius must be positive')
-
-    #Regulate glon input to make the spherical wraparound less annoying
-    if glon <= 360 and glon >= 295.5:
-        glon = glon - 360
-
-    #Get array shape
-    rows = array.shape[0]
-    cols = array.shape[1]
-
-    #Set up the degree range for glon and glat based on the array shape
-    glat_range = np.linspace(1,-1,rows)
-    glon_range = np.linspace(64.5, -64.5, cols)
-
-    #Get the index based on closes value
-    glat_idx = (np.abs(glat_range - glat)).argmin()
-    glon_idx = (np.abs(glon_range - glon)).argmin()
-
-    #Get the effective pixel radius
-    radius_in_pixels = int(reff * 3000)
-
-    return (glon_idx, glat_idx, radius_in_pixels)
 
 #Initialize dictionary to store same bubble IDs with their new converted values
 converted_bubble_dict = {}
@@ -191,88 +256,3 @@ for name,value in converted_bubble_dict.items():
 
     #Fill the control dictionary
     control_dict[name] = (control_cutout, radius, hitrate, control_center)
-
-
-
-
-def show_cutout_samples(cutout_dictionary, show_best, num_samples=12):
-    '''
-    Returns:
-    -------------
-    Figure with cutout samples
-
-    Args:
-    -------------
-    cutout_dictionary: dictionary; contains names as keys and numeric tuple as values
-    show_best: bool; if True, will show assortment of high hitrate bubbles
-                     if Fasle, will show random assortment of bubbles
-    num_samples: int; number of samples to show. Currently fixed at 12 for convenience
-
-    Usage:
-    -------------
-    show_cutout_samples(cutout_dict, show_best=True)
-
-    ''' 
-    
-    #Initialize lists to store dictionary information
-    cutout_names = []
-    cutout_images = []
-    cutout_radii = []
-    cutout_hitrates = []
-
-    #Set hitrate threshold value for show_best restriction
-    #Note: make sure at least 12 valid bubbles can be found...
-    threshold = 0.5
-    
-    #Fill the lists depending on value of show_best
-    for bub_name, bub_num in cutout_dictionary.items():
-        if show_best == True:
-            if bub_num[2] >= threshold:
-                cutout_names.append(bub_name)
-                cutout_images.append(bub_num[0])
-                cutout_radii.append(bub_num[1])
-                cutout_hitrates.append(bub_num[2])
-            else:
-                continue
-        else:
-            cutout_names.append(bub_name)
-            cutout_images.append(bub_num[0])
-            cutout_radii.append(bub_num[1])
-            cutout_hitrates.append(bub_num[2])
-
-    #Set up the figure 
-    fig = plt.figure()
-    if show_best == True:
-        title = str(num_samples) + " Samples with High Hitrate" 
-    else:
-        title = str(num_samples) + " Cutout Samples"
-    fig.suptitle(title, fontsize=18, x=0.5, y=0.986)
-    fig.subplots_adjust(hspace = 0.75)
-
-    #Plot the sample cutouts
-    for sample in range(1,13):
-        fig.add_subplot(3,4,sample)
-        plt.imshow(cutout_images[sample])
-        plot_title = cutout_names[sample] + "\n" + "(hitrate = " + str(cutout_hitrates[sample]) + ")"
-        plt.title(plot_title, fontsize=6)
-    print("Exit figure to continue...")
-    plt.show()
-
-
-'''For sample cutout display
-show_cutout_samples(cutout_dict, show_best=False)
-
-show_cutout_samples(cutout_dict, show_best=True)
-'''
-
-
-'''Correlation between radius and hitrate
-radii = []
-hr = []
-for info in cutout_dict.values():
-    radii.append(info[1])
-    hr.append(info[2])
-
-plt.scatter(radii,hr)
-plt.show()
-'''
